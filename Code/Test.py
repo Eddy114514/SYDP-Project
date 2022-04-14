110aa81967b9e355b7c81600c6a1d9ad1dd5f965
Eddy810 <01234567890eddy@gmail.com>
2022-04-14 17:00:14 -0400

    Test

--- Code/Test.py
+++ Code/Test.py
@@ -1,7 +1,22 @@
+import numpy
+from stl import mesh
 
+# Using an existing stl file:
 
-flist = [lambda x:x, lambda x:x]
-f = lambda x: (flist[0](x)*flist[1](x))
+# Or creating a new mesh (make sure not to overwrite the `mesh` import by
+# naming it `mesh`):
+VERTICE_COUNT = 100
+data = numpy.zeros(VERTICE_COUNT, dtype=mesh.Mesh.dtype)
+your_mesh = mesh.Mesh(data, remove_empty_areas=False)
 
+# The mesh normals (calculated automatically)
+your_mesh.normals
+# The mesh vectors
+your_mesh.v0, your_mesh.v1, your_mesh.v2
+# Accessing individual points (concatenation of v0, v1 and v2 in triplets)
+assert (your_mesh.points[0][0:3] == your_mesh.v0[0]).all()
+assert (your_mesh.points[0][3:6] == your_mesh.v1[0]).all()
+assert (your_mesh.points[0][6:9] == your_mesh.v2[0]).all()
+assert (your_mesh.points[1][0:3] == your_mesh.v0[1]).all()
 
-print(f(2))
+your_mesh.save('new_stl_file.stl')
